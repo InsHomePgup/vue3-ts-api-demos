8c7bcd08f4559d9f4df911c0d9a5f78b1b2774bf
Abraham K. <47906083+InsHomePgup@users.noreply.github.com>
2024-12-23 09:49:56 +0800

    ts class

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
-export const myName = 'foo'
-const obj:object={
-    foo:12
-}
+// export const myName = 'foo'
+// const obj:object={
+//     foo:12
+// }
+//
+// let num :number = 3;
+//
+// Object.keys(obj).forEach((key:string) => {
+//     console.log(key)
+// })
+//
+//
+// interface PersonDto {
+//     name: string
+//     age: number
+//     job?:string
+//     salary?:number
+//     readonly gender ?:0|1
+// }
+import Person from './person';
+console.log(Person);
+let Bob = new Person('Bob',20);
+console.log(Bob);
+// Bob.gender = 0;
+Bob.setGender(0);
+console.log(Bob);
+console.log(Bob.gender)
+const BobGender = Bob.getGender();
+console.log(BobGender)
 
-let num :number = 3;
-
-Object.keys(obj).forEach((key:string) => {
-    console.log(key)
-})
---
+++ person.ts
@@ -0,0 +1,36 @@
+import * as _ from 'lodash-es';
+type genderDto = 0|1
+
+export default class Person {
+    name: string;
+    age: number;  // 普通属性
+    // gender?:genderDto;  // 只读属性
+    constructor(name: string, age: number,gender?:genderDto) {  // 构造函数
+        this.name = name;
+        this.age = age;
+        // this.gender = gender;
+    }
+    clonePerson(person:Person){
+        return new Person(person.name,person.age);
+    }
+
+    // private set gender(gender:genderDto){
+    //     this.gender = gender;
+    // }
+    //
+    // private get gender(){
+    //     return this.gender;
+    // }
+
+    setGender(gender:genderDto){
+        this.gender = gender;
+    }
+
+    getGender(){
+        if(_.isNumber(this.gender)){
+            return this.gender===0?'男':'女'
+        }else{
+            return null
+        }
+    }
+}
