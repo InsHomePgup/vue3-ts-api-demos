bfe3afe5925b53dbd16a13fc70e191bbf8eda657
Abraham K. <47906083+InsHomePgup@users.noreply.github.com>
2024-12-25 14:44:07 +0800

    vue3

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Vue App
+        <div class="w-10 h-10 bg-red"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
+        vue(),
         UnoCSS(),
     ],
+    server: {
+        port: 5150,
+        host: '0.0.0.0'
+    },
 })
