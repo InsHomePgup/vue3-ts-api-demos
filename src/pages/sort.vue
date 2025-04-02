eb969c0a695538dd3a378850128272ee308ad9c4
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-04-02 13:29:54 +0800

    Revert "feat: a template"
    This reverts commit 5fc93a9bd5013a93b3111293121e42634b6e6394.

---
+++ src/pages/sort.vue
@@ -0,0 +1,14 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import _ from 'lodash-es'
+
+const res = _.sortBy([{ a: 1 }, { a: 4 }, { a: 2 }, { a: 3 }], ['a'])
+console.log(res)
+</script>
+
+<style scoped>
+
+</style>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,30 +1,19 @@
 // stores/counter.ts
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', {
+interface CounterState {
+  count: number
+}
+
+export const useCounterStore = defineStore<'counter', CounterState>('counter', {
   state: () => {
     return { count: 0 }
   },
-  getters: {
-    doubleCount(state): number {
-      return state.count * 2
-    },
-    isEven(state): boolean {
-      return state.count % 2 === 0
-    },
-  },
+  // could also be defined as
+  // state: () => ({ count: 0 })
   actions: {
     increment() {
       this.count++
-    },
-    decrement() {
-      this.count--
-    },
-    reset() {
-      this.count = 0
-    },
-    setCount(value: number) {
-      this.count = value
     },
   },
 })
