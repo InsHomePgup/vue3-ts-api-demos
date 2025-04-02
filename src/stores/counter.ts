a9d00da0e9c207d6943584447d95db9cb934bea1
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-04-02 13:30:52 +0800

    -

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,16 +1,9 @@
-// stores/counter.ts
 import { defineStore } from 'pinia'
 
-interface CounterState {
-  count: number
-}
-
-export const useCounterStore = defineStore<'counter', CounterState>('counter', {
+export const useCounterStore = defineStore('counter', {
   state: () => {
     return { count: 0 }
   },
-  // could also be defined as
-  // state: () => ({ count: 0 })
   actions: {
     increment() {
       this.count++
